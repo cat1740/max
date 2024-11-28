ab65f26c7023bc290c515c16e1bacc60980f0c01
Ala-UA-drakon-UA-Ala <andrewvin13a@gmail.com>
2024-11-28 20:14:59 +0100

    Вивчення змінних

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 print("hello ")
 print("hi")
+
+name = "Max"   #string (str)
+number  = 5    #int
+number2 = 5.3  #float
+number3 = '7'  #str
+maybes = False  #bool   True (правда) /False (ложь)
+
+print("result = 3")
+print("result =", number, name)
+print("result =", number,'+', number2)
+#print("result =", number + number3)
+
+print("result =", str(number),  number3)
+
+number4 = float(input('number1:')) #10.5
+number5 = int(input('number2:')) #2.5
+textA = input('name:') #Max
+
+print(number4 + number5, textA)
+
+# 1. number4 + number5
+# 2. '1' + textA
+# 3.  print
+
+
+
+
+#if number == 5:
+#    print('result = 5')
+#    if number != 5:
+#        print('result  not5')
